5ed2ddcfae304fd9aa62e6dd14db27fa33a662c9
beyond88 <muhin.cse.diu@gmail.com>
2022-12-18 19:01:49 +0600

    added custom post status

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,5 +1,30 @@
-function($) {
+(function($) {
     'use strict';
 
-    
+
+    var $postStatusSelect = $('#post-status-select');
+
+    var postStatus = $('#hidden_post_status');
+    $('#post-status-display').text(postStatus.val());
+
+    var box = $postStatusSelect.find('.save-post-status');
+    if (typeof box === 'object' && box !== null && 'addEventListener' in box) {
+        box.addEventListener('click', function onClick() {
+            console.log('box clicked');
+        });
+    }
+    // $postStatusSelect.find('.save-post-status').on( 'click', function( event ) {
+       
+    //     if ( $('#post_status option:selected').val() == 'pending' ) {
+    //         $('#save-post').show().val('Save as Pending');
+    //     } else if ( $('#post_status option:selected').val() == 'approved' ) {
+    //         $('#save-post').show().val('Save as Approved');
+    //     } else if ( $('#post_status option:selected').val() == 'suspend' ) {
+    //         $('#save-post').show().val('Save as Suspend');
+    //     } else {
+    //         $('#save-post').show().val('Save Draft');
+    //     }
+
+    //     event.preventDefault();
+    // });
 })(jQuery);
---
+++ includes/functions.php
@@ -0,0 +1,81 @@
+<?php 
+
+function givekindness_campaign_status(){
+	register_post_status( 'approved', array(
+		'label'                     => _x( 'Approved', 'give_forms' ),
+		'label_count'               => _n_noop( 'Approved <span class="count">(%s)</span>', 'Approved <span class="count">(%s)</span>'),
+		'public'                    => true,
+		'exclude_from_search'       => false,
+		'show_in_admin_all_list'    => true,
+		'show_in_admin_status_list' => true
+	));
+
+    register_post_status( 'suspend', array(
+		'label'                     => _x( 'Suspend', 'give_forms' ),
+		'label_count'               => _n_noop( 'Suspend <span class="count">(%s)</span>', 'Suspend <span class="count">(%s)</span>'),
+		'public'                    => true,
+		'exclude_from_search'       => false,
+		'show_in_admin_all_list'    => true,
+		'show_in_admin_status_list' => true
+	));
+}
+add_action( 'init', 'givekindness_campaign_status' );
+
+function givekindness_status_add_in_quick_edit() {
+
+    global $post;
+    if($post->post_type == 'give_forms') {
+
+	echo "<script>
+        jQuery(document).ready( function() {
+            jQuery( 'select[name=\"_status\"]' ).append( '<option value=\"approved\">Approved</option>' ); 
+            jQuery( 'select[name=\"_status\"]' ).append( '<option value=\"suspend\">Suspend</option>' );     
+        });
+	</script>";
+    }
+
+}
+add_action('admin_footer-edit.php','givekindness_status_add_in_quick_edit');
+
+function givekindness_status_add_in_post_page() {
+
+    global $post;
+    if($post->post_type == 'give_forms') {
+        echo "<script>
+        jQuery(document).ready( function() {        
+            jQuery( 'select[name=\"post_status\"]' ).append( '<option value=\"approved\">Approved</option>' );
+        });
+        </script>";
+    }
+
+    if($post->post_type == 'give_forms') {
+        echo "<script>
+        jQuery(document).ready( function() {        
+            jQuery( 'select[name=\"post_status\"]' ).append( '<option value=\"suspend\">Suspend</option>' );
+        });
+        </script>";
+    }
+}
+add_action('admin_footer-post.php', 'givekindness_status_add_in_post_page');
+add_action('admin_footer-post-new.php', 'givekindness_status_add_in_post_page');
+
+add_filter( 'display_post_states', function( $statuses ) {
+    global $post;
+
+    if( $post->post_type == 'give_forms') {
+
+        if ( get_query_var( 'post_status' ) != 'approved' ) { // not for pages with all posts of this status
+            if ( $post->post_status == 'approved' ) {
+                return array( 'Approved' );
+            }
+        }
+
+        if ( get_query_var( 'post_status' ) != 'suspend' ) { // not for pages with all posts of this status
+            if ( $post->post_status == 'suspend' ) {
+                return array( 'Suspend' );
+            }
+        }
+        
+    }
+    return $statuses;
+});
