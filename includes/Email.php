1f436db0828c8d33d924ca3409ee7e7784a0ce69
beyond88 <muhin.cse.diu@gmail.com>
2023-02-07 17:54:22 +0600

    Email class created

---
+++ includes/Email.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Give_Kindness;
+
+/**
+ * Email handlers class
+ */
+class Email {
+
+
+    /******
+     * 
+     * @params none
+     * @return void
+     */
+    public function __construct( ){
+
+    }
+
+    
+
+}
