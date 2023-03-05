aa7e0bd676f5a9578f3dc14c3396083d50798afa
beyond88 <muhin.cse.diu@gmail.com>
2023-03-05 13:07:54 +0600

    Fix margin bottom issue

--- includes/Templates/update-campaign/fundraisers.php
+++ includes/Templates/update-campaign/fundraisers.php
@@ -1,4 +1,12 @@
 <div class="give-donor-dashboard-tab-content" id="give_kindness-campaign-fundraisers" data-tab-content="give_kindness-campaign-fundraisers">
+    <div class="give-donor-dashboard-heading" style="display:block;">
+        <h4 class='gk-margin-bottom-10'>
+            <?php echo __('Fundraisers', 'give-kindness'); ?>
+        </h4>
+        <p class='gk-margin-bottom-10'> You can add more family friends as fundraisers for this campaign. It's always better with a team! </p>
+   </div>
+   <div class="give-donor-dashboard-divider"></div>
+
     <div class="give-donor-dashboard-field-row">
         <div class="give-donor-dashboard-text-control">
             <label class="give-donor-dashboard-text-control__label" for="gke-campaign-email">
