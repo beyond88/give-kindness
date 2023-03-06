17bf8b52086c91a8b75d8c15f68e2780588c4eda
beyond88 <muhin.cse.diu@gmail.com>
2023-03-06 13:00:10 +0600

    show goal, revenue, total donations in campaign list

---
+++ includes/Templates/update-campaign/chat-presets.php
@@ -0,0 +1,3 @@
+<div class="give-donor-dashboard-tab-content" id="give_kindness-campaign-chat" data-tab-content="give_kindness-campaign-chat">
+    Coming soon
+</div>
---
+++ includes/Templates/update-campaign/docs.php
@@ -0,0 +1,3 @@
+<div class="give-donor-dashboard-tab-content" id="give_kindness-campaign-docs" data-tab-content="give_kindness-campaign-docs">
+    Coming soon
+</div>
