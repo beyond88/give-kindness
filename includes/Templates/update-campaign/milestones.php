79bff6d74eae8d75c8b69f5cd91f2e2c17f399eb
beyond88 <muhin.cse.diu@gmail.com>
2023-03-12 19:09:58 +0600

    show milestone page

--- includes/Templates/update-campaign/milestones.php
+++ includes/Templates/update-campaign/milestones.php
@@ -1,3 +1,26 @@
 <div class="give-donor-dashboard-tab-content" id="give_kindness-campaign-milestones" data-tab-content="give_kindness-campaign-milestones">
-    Coming soon
+    <h4 class='gk-margin-bottom-10'>
+        <?php echo __('Milestones', 'give-kindness'); ?>
+    </h4>
+    <div class="give-donor-dashboard-divider"></div>
+    <div class="give-donor-dashboard-field-row">
+        <div class="give-donor-dashboard-text-control">
+            <label class="switch">
+                <input class="widefat" id="disable_limit_per_order" type="checkbox" name="woo_free_product_sample_settings[disable_limit_per_order]" value="1">
+                <div class="slider"></div>
+            </label>
+        </div>
+    </div>
+    <div class="give-donor-dashboard-field-row">
+        <div class="give-donor-dashboard-text-control">
+            <label class="give-donor-dashboard-text-control__label">
+                <?php echo __('Fundraising target', 'give-kindness'); ?>
+            </label>
+            $10000
+        </div>
+
+        <div class="give-donor-dashboard-text-control__input">
+            
+        </div>
+    </div>
 </div>
