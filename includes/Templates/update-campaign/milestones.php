b1f0c4277879b2f146e0d8b7099a4c68fe561f1d
beyond88 <muhin.cse.diu@gmail.com>
2023-03-13 12:23:41 +0600

    Show milestone fields

--- includes/Templates/update-campaign/milestones.php
+++ includes/Templates/update-campaign/milestones.php
@@ -5,22 +5,44 @@
     <div class="give-donor-dashboard-divider"></div>
     <div class="give-donor-dashboard-field-row">
         <div class="give-donor-dashboard-text-control">
-            <label class="switch">
-                <input class="widefat" id="disable_limit_per_order" type="checkbox" name="woo_free_product_sample_settings[disable_limit_per_order]" value="1">
-                <div class="slider"></div>
+            <label class="give-kindness-switch">
+                <input class="widefat" id="give-kindness-milestone-switch" type="checkbox" name="give-kindness-milestone-switch" value="1">
+                <div class="give-kindness-slider"></div>
             </label>
         </div>
     </div>
-    <div class="give-donor-dashboard-field-row">
+    <div class="give-donor-dashboard-field-row gk-margin-top-10 give-kindness-milestone-hide">
         <div class="give-donor-dashboard-text-control">
             <label class="give-donor-dashboard-text-control__label">
                 <?php echo __('Fundraising target', 'give-kindness'); ?>
             </label>
-            $10000
-        </div>
-
-        <div class="give-donor-dashboard-text-control__input">
-            
+            <h4 class="give-kindness-milestone-label" id="give-kindness-milestone-label">
+                $10000
+            </h4>
         </div>
     </div>
+
+    <div class="give-kindness-milestone-operate give-kindness-milestone-hide" id="give-kindness-milestone-operate">
+        <div class="give-donor-dashboard-field-row">
+            <div class="give-donor-dashboard-text-control">
+                <label class="give-donor-dashboard-text-control__label" for="gk-milestone-goal">
+                    <?php echo __('Milestone GOAL', 'give-kindness'); ?>
+                </label>
+                <div class="give-donor-dashboard-text-control__input">
+                    <input id="gk-milestone-goal" name="gk-milestone-goal[]" type="text" placeholder="<?php echo __('$1000', 'give-kindness'); ?>"  maxlength="20">
+                </div>
+            </div>
+            <div class="give-donor-dashboard-text-control">
+                <label class="give-donor-dashboard-text-control__label" for="gk-milestone-goal">
+                    <?php echo __('Label (40 characters)', 'give-kindness'); ?>
+                </label>
+                <div class="give-donor-dashboard-text-control__input">
+                    <input id="gk-milestone-goal-label" name="gk-milestone-goal-label[]" type="text" placeholder="<?php echo __('Final Milestone', 'give-kindness'); ?>"  maxlength="40">
+                </div>
+            </div>
+        </div>
+        <button class="give-donor-dashboard-button give-donor-dashboard-button--primary" id="give-kindness-milestone-add">
+            <?php echo __('Add', 'give-kindness'); ?>
+        </button>
+   </div>
 </div>
